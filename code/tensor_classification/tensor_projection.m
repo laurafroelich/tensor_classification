66cf66ea56a2515663f2cc2d1f65bce8b6be8f6e
laurafroelich <laura.frolich@gmail.com>
2018-02-25 06:32:31 +0100

    added possibility to pass an array of observations instead of a cell array to tensor_projection and get an array instead of a cell array as a return value

--- code/tensor_classification/tensor_projection.m
+++ code/tensor_classification/tensor_projection.m
@@ -1,29 +1,45 @@
 function Xprojections = tensor_projection(Xs, Us)
 
-nsamples = length(Xs);
 nmodes = length(Us);
-origdims = size(Xs{1});
-Xprojections = cell(nsamples, 1);
+
+is_cell_data = iscell(Xs);
+
+if is_cell_data
+    nsamples = length(Xs);
+    Xprojections = cell(nsamples, 1);
+else
+    Xdim = size(Xs);
+    nsamples = Xdim(end);
+    y_mat_size = NaN(1, nmodes+1);
+    for imode = 1:nmodes
+        Udims = size(Us{imode});
+        y_mat_size(imode) = Udims(1);
+    end
+    y_mat_size(nmodes+1) = nsamples;
+    Xprojections = NaN(y_mat_size);
+    Xs = permute(Xs, [nmodes+1, 1:nmodes]);
+    Xprojections = permute(Xprojections, [nmodes+1, 1:nmodes]);
+end
 
 for isample = 1:nsamples
-    Xprojection = Xs{isample};
+    if is_cell_data
+        Xprojection = Xs{isample};
+    else
+        Xprojection = Xs(isample,:);
+    end
     
     for jmode = 1:nmodes % do multiplication between Xi tensor and U matrices
         Xprojection = tmult(Xprojection, Us{jmode}', jmode);
     end
-    Xprojections{isample} = Xprojection;
-    
-    
-    if false % equivalent to using tmult, can be used if tmult is not available
-        dims = origdims;
-        for jmode = 1:nmodes % do multiplication between Xi tensor and U matrices
-            Xprojection = Us{jmode}'*matricizing(Xprojection, jmode);
-            dims(jmode) = size(Us{jmode}, 2);
-            Xprojection = unmatricizing(Xprojection, jmode, dims);
-            Xprojections{isample} = Xprojection;
-        end;
+    if is_cell_data
+        Xprojections{isample} = Xprojection;
+    else
+        Xprojections(isample,:)=Xprojection;
     end
     
+    if ~is_cell_data
+        Xprojections = permute(Xprojections, [2:(n_modes+1), 1]);
+    end
     
 end
 
