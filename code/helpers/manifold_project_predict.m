4ef73fd711efb270b430c66ad960b0ffb7f9d31c
laurafroelich <laura.frolich@gmail.com>
2018-02-27 21:40:53 +0100

    fixed typo in function call

--- code/helpers/manifold_project_predict.m
+++ code/helpers/manifold_project_predict.m
@@ -52,7 +52,7 @@ Us = results{best_solution}.Us;
 if isequal(fun, @ManPDA_normsratio) || isequal(fun, @ManPDA)
 predictions = project_and_predict(x_train, x_test, y_train, y_test, ncomps, Us, true);
 else
-predictions = project_and_predict(x_train, x_test, y_train, y_test, ncomps, Usxs);    
+predictions = project_and_predict(x_train, x_test, y_train, y_test, ncomps, Us);    
 end
 
 [~, ~, ~, auc] = perfcurve(y_test, predictions(:, 2), 2);
