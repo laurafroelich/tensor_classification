c115d366d93eb3b59f370c91144828f85e120704
laurafroelich <laura.frolich@gmail.com>
2018-02-27 19:04:55 +0100

    added a description of the function manifold_project_predict

--- code/helpers/manifold_project_predict.m
+++ code/helpers/manifold_project_predict.m
@@ -1,5 +1,27 @@
 function auc = manifold_project_predict(fun, x_train, x_test, ...
 y_train, y_test, ncomps)
+% auc = manifold_project_predict(fun, x_train, x_test, y_train, y_test, ncomps)
+%
+% Using one of the four projection methods optimised on the Stiefel
+% manifold (@ManTDA_normsratio, @ManPDA_normsratio ', '@ManTDA, or
+% @ManPDA), learn a projection, use this projection to project training and
+% test data onto a lower dimensional representation. Then use these
+% representations from the training data to train a multinomial regression
+% classifier, and apply the classifier to the test data. Return the Area
+% Under ROC Curve from the test data.
+%
+% Input:
+%
+% fun: manifold projection method (@ManTDA_normsratio, @ManPDA_normsratio ', 
+%      '@ManTDA, or @ManPDA)
+% x_train: cell array with one training observation in each cell.
+% x_test: cell array with one test observation in each cell.
+% y_train: vector with class labels. Only two classes currently supported,
+%          these must be the labels 1 or 2.
+% y_test: vector with class labels. Only two classes currently supported,
+%          these must be the labels 1 or 2.
+% ncomps: integer. Number of components in each mode.
+
 nrandinits = 5;
 objfuncvals = NaN(1, nrandinits);
 results = cell(1, nrandinits);
