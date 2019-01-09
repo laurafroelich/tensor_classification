787b8f7ba0a5927cb07dad64c189d8e3f0d74a64
laurafroelich <laura.frolich@gmail.com>
2019-01-09 20:28:43 +0100

    Adding test for gradient in LDA PARAFAC with respect to U2.

--- matlab/code/tests/test_gradients.m
+++ matlab/code/tests/test_gradients.m
@@ -6,34 +6,39 @@ function test_FunctionOne(testCase)
     verifyEqual(testCase, 4, 4)
 end
 
-function test_analytical_vs_numerical_gradient(testCase)
+function test_parafac_lda_gradient(testCase)
+    analytical_vs_numerical_gradient(testCase, 'U1',...
+        @parafacldaobj_matrixdata)
+    analytical_vs_numerical_gradient(testCase, 'U2',...
+        @parafacldaobj_matrixdata)
+end
+
+function analytical_vs_numerical_gradient(testCase, matrix_name, ...
+    objective_function)
     p = 5;
     q = 7;
-    k1 = 1;
-    k2 = 1;
     nsamples = 100;
     [Xs, ys] = get_data(nsamples, [p, q]);
+    
+    [classmeandiffs, observationdiffs, nis] = ...
+        classbased_differences(Xs, ys);
+    
+    k1 = 3;
+    k2 = 3;
+    myfun = @(U) objective_function(U,...
+        classmeandiffs, observationdiffs, nis, k2, k2);
+
     initial_matrices_struct = get_initial_matrices(p, q, k1, k2);
-    
-    [classmeandiffs, observationdiffs, nis] = classbased_differences(Xs, ys);
-    
-
-    [Fp, G, classmeandiffstensor, observationdiffstensor] = parafacldaobj_matrixdata(initial_matrices_struct,...
-        classmeandiffs, observationdiffs, nis, k1, k2);
-    
-    myfun = @(U)...
-        parafacldaobj_matrixdata(U,...
-        classmeandiffs, observationdiffs, nis, k2, k2, ...
-        classmeandiffstensor, observationdiffstensor);
-
     numerically_estimated_gradient = get_numerically_estimated_gradient(...
-        myfun, initial_matrices_struct);
+        myfun, initial_matrices_struct, matrix_name);
     
     [F, analyticalderiv] = myfun(initial_matrices_struct);
     
     actual_difference = norm(...
-        numerically_estimated_gradient-analyticalderiv.U1, 'fro')/...
-        norm(analyticalderiv.U1, 'fro');
+        numerically_estimated_gradient-...
+        analyticalderiv.(matrix_name), 'fro')/...
+        norm(analyticalderiv.(matrix_name), 'fro');
+    
     verifyEqual(testCase, 0, actual_difference, 'AbsTol', 1e-5)
 end
 
@@ -79,23 +84,25 @@ function [Xs, classes] = get_data(nsamples, xdims)
     
 end
 
-function estderiv = get_numerically_estimated_gradient(myfun, Ustruct)
+function estderiv = get_numerically_estimated_gradient(myfun, ...
+    Ustruct, matrix_name)
 
-    [I, J] = size(Ustruct.U1);
+    [I, J] = size(Ustruct.(matrix_name));
     myeps = 1e-9;
     estderiv = zeros(I, J);
 
     for i=1:I
         for iparam=1:J
-            wp = Ustruct.U1;
-            wm = Ustruct.U1;
+            current_u = Ustruct.(matrix_name);
+            wp = current_u;
+            wm = current_u;
             wp(i, iparam) = wp(i, iparam) + myeps;
             wm(i, iparam) = wm(i, iparam) - myeps;
-            Ustruct.U1 = wp;
+            Ustruct.(matrix_name) = wp;
 
             Fp = myfun(Ustruct);
 
-            Ustruct.U1 = wm;
+            Ustruct.(matrix_name) = wm;
             Fm = myfun(Ustruct);
             estderiv(i, iparam) = (Fp-Fm)/(2*myeps);
         end
