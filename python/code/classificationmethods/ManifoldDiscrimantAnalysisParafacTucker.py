bb1a7e715b5745b5d45769d06e2c69fd22c9966c
unknown <emilfriisfrolich@gmail.com>
2019-03-22 21:49:08 +0100

    Fixed a lot of bugs due to inconsistent data structures

--- python/code/classificationmethods/ManifoldDiscrimantAnalysisParafacTucker.py
+++ python/code/classificationmethods/ManifoldDiscrimantAnalysisParafacTucker.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 import numpy as np
+from scipy.stats import ortho_group  
+
 
 class ManifoldDiscrimantAnalysis(ABC):
     def __init__(self, Xs, classes, lowerdims=None, Us=None, opts=None, usestoppingcrit=True, maxits=1000, store={'Rw': 0, 'Rb': 0, 'QtRb': 0, 'QtRw':  0, 'QtBQ': 0, 'QtWQ': 0, 'QtWQinvQtBQ': 0}, optmeth='ManOpt'):
@@ -17,22 +19,43 @@ class ManifoldDiscrimantAnalysis(ABC):
         if optmeth not in ['bo13', 'wen12', 'ManOpt']:
             self.optmeth='ManOpt'
             print("Chosen optimization method", optmeth, "has not been implemented")
+        self.Fdifftol=Fdifftol=10**(-10)
+        self.Udifftol=Udifftol=10**(-12)
+        self.Xsample1 = Xs[0];
+
+        self.sizeX = np.shape(self.Xsample1);
+
+        self.nmodes = len(self.sizeX);
+
+        self.nsamples = np.shape(Xs);
+        if Us==None:
+            for i in range(self.nmodes):
+                Us[i]=ortho_group.rvs(dim=(np.shape(self.sizeX)[i])) #Contemplate changing this
+
+        
     def SetTolerances(self,Fdifftol=10**(-10), Udifftol=10**(-12)):
         self.Fdifftol=Fdifftol
         self.Udifftol=Udifftol
         
     @abstractmethod 
-    def ObjectMatrixData(x, store, classmeandiffs, observationdiffs, nis, K1, K2, Rw, Rb): #This is a virtual function that any inheriting subclass should realize
+    def ObjectMatrixData(self): #This is a virtual function that any inheriting subclass should realize
         return(None)
         
     def MyCost(self, x, store, classmeandiffs, observationdiffs, nis, K1, K2, Rw, Rb):
-        self.F, self.store=self.ObjectMatrixData(x, store, classmeandiffs, observationdiffs, nis, K1, K2, Rw, Rb)
-   
-     
+        self.ObjectMatrixData(x, store, classmeandiffs, observationdiffs, nis, K1, K2, Rw, Rb)
+    def MyGrad():
+        self.ObjectMatrixData(self):
+            """
+            This is the logical place to implement the function calculating G
+            """
+     def ClassBasedDifferences(self):
+         """
+         We should implement the classbased_differences method from the MATLAB code here. 
+         """
 class TuckerDiscriminantAnalysis(ManifoldDiscrimantAnalysis):
     def QtCheck(self,Qt):
         if self.store[Qt]==0:
-            store[Qt]=QtCalculator(Qt)
+            self.store[Qt]=self.QtCalculator(Qt)
     def QtCalculator(self,Qt,N=None,K1=None,K2=None,U1=None,U2=None):
             if Qt in {'QtRw', 'QtRb'}:
                 Qt_mm=np.tensordot(np.tensordot(self.Qt[:-2],np.linalg.transpose(U1),axes=(1,0)),np.linalg.transpose(U2),axes=(2,0)) #Something might be horribly wrong here...
@@ -45,26 +68,24 @@ class TuckerDiscriminantAnalysis(ManifoldDiscrimantAnalysis):
             else: 
                 print("Wrong Qt specified")
             
-    def ObjectMatrixData(U, classmeandiffs, observationdiffs, nis, K1, K2, Rw=None, Rb=None, store=None):
-        if ('Rw' or 'Rb') not in store:
+    def ObjectMatrixData(self, U, classmeandiffs, observationdiffs, nis, K1, K2):
+        if (self.store['Rw']==0 or self.store['Rb']==0):
             obsExample=classmeandiffs[0]
             sizeObs=np.shape(obsExample)
             I=sizeObs[0]
             J=sizeObs[1]
-            if Rw==None: #What if only one of them is missing? Shouldn't we still calculate the missing one? Split the code below into two parts, one for each case. This has been done now, but not in the MATLAB code
+            if self.store['Rw']==0: #What if only one of them is missing? Shouldn't we still calculate the missing one? Split the code below into two parts, one for each case. This has been done now, but not in the MATLAB code
                 nclasses=max(np.shape(classmeandiffs)) #This is a straight-copy of the existing MATLAB code, not sure if this is the intended behavior there either...
                 classmeandiffstensor=np.reshape(classmeandiffs,(I,J,nclasses),order="F")
                 Rb=classmeandiffstensor
-            if Rw==None:
+            if self.store['Rb']==0:
                 nobs=max(np.shape(observationdiffs))
                 observationdiffstensor=np.reshape(observationdiffs,(I,J,nobs),order="F")
                 Rw=observationdiffstensor
-            store['Rw'] = Rw
-            store['Rb'] = Rb
-        Rw=store['Rw'] #This assignment SHOULD be superflous now...
-        Rw=store['Rb']
-        
-        
+            self.store['Rw'] = Rw
+            self.store['Rb'] = Rb
+#        Rw=store['Rw'] #This assignment SHOULD be superflous now...
+#        Rw=store['Rb']
         Rwsize = np.shape(Rw)
         nobs=Rwsize[-1]
         datadims=np.shape(Rb)
@@ -73,10 +94,12 @@ class TuckerDiscriminantAnalysis(ManifoldDiscrimantAnalysis):
         M=datadims[1]
         #We proceed to calculate all relevant matrices for the optimization step. 
         for j in {'QtRw', 'QtRb', 'QtWQ', 'QtBQ', 'QtWQinvQtBQ'}:
-            TuckerDiscriminantAnalysis.QtCheck(j)
+            self.QtCheck(j)
         
-        F=np.linalg.trace(store['QtWQinvQtBQ'])
-        return(F, G, Rw, Rb, store)
+        F=np.linalg.trace(self.store['QtWQinvQtBQ'])
+        
+        
+        return(F, G) 
         
         
 class PARAFACDiscriminantAnalysis(ManifoldDiscrimantAnalysis):    
