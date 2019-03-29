974044fe7f47d2fef9d13bdbec98c2615857f1f1
unknown <emilfriisfrolich@gmail.com>
2019-03-29 12:14:24 +0100

    Removed all pycharm deteccted errors

--- python/code/classificationmethods/ManifoldDiscrimantAnalysisParafacTucker.py
+++ python/code/classificationmethods/ManifoldDiscrimantAnalysisParafacTucker.py
@@ -5,12 +5,12 @@ from pymanopt import Problem
 from pymanopt.manifolds import Product, Stiefel
 import tensorflow as tf
 from pymanopt.solvers import ConjugateGradient
+from sklearn.pipeline import Pipeline
 
-
-class ManifoldDiscrimantAnalysis(ABC,Pipe):
+class ManifoldDiscrimantAnalysis(ABC,Pipeline):
     def __init__(self, classes, Us=None, opts=None, usestoppingcrit=True, maxits=1000,
-                 store={'Rw': None, 'Rb': None, 'QtRb': None, 'QtRw':  None, 'QtBQ': None, 'QtWQ': None, 'QtWQinvQtBQ': None, 'FdwrtQ': None},
-                 optmeth='ManOpt', MyCost=None):
+                 store={'Rw': None, 'Rb': None, 'QtRb': None, 'QtRw':  None, 'QtBQ': None, 'QtWQ': None, 'QtWQinvQtBQ': None},
+                 optmeth='ManOpt'):
         self.classes=classes
         self.Us=Us
         self.opts=opts
@@ -23,20 +23,10 @@ class ManifoldDiscrimantAnalysis(ABC,Pipe):
             print("Chosen optimization method", optmeth, "has not been implemented")
         self.Fdifftol=Fdifftol=10**(-10)
         self.Udifftol=Udifftol=10**(-12)
-        self.Xsample1 = Xs[0];
-
-        self.sizeX = np.shape(self.Xsample1);
-
-        self.nmodes = len(self.sizeX);
-
-        self.nsamples = np.shape(Xs);
-        self.F=0
-        self.G={'U1': 0, 'U2': 0}
         if Us==None:
             for i in range(self.nmodes):
                 Us[i]=ortho_group.rvs(dim=(np.shape(self.sizeX)[i])) #Contemplate changing this
-
-        
+        self.MyCost=None
     def SetTolerances(self,Fdifftol=10**(-10), Udifftol=10**(-12)):
         self.Fdifftol=Fdifftol
         self.Udifftol=Udifftol
@@ -56,7 +46,7 @@ class ManifoldDiscrimantAnalysis(ABC,Pipe):
         #self.F=-np.linalg.trace(self.store['QtWQinvQtBQ'])
         Q = tf.Variable(tf.placeholder(tf.float32))
         self.MyCost = tf.linalg.trace(store['QtWQinvQtBQ'])
-        problem = Problem(manifold=manifold, cost=MyCost, arg=Q)
+        #problem = Problem(manifold=manifold, cost=MyCost, arg=Q)
         """
         With tensorflow:
             Q=tf.Variable(tf.placeholder(tf.float32))
@@ -68,7 +58,7 @@ class ManifoldDiscrimantAnalysis(ABC,Pipe):
         
     def MyGrad(self):
 
-            """
+        """
             Matlab code implementing the calculation of the gradient: 
                 Let's contemplate doing this in TensorFlow. It just requires us to unwrap all of the construction of QtWQinvQtBQ. 
             TTT=reshape(permute(reshape(FdwrtQ, M, N, 1, K), [2 4 1 3]),[N*K M]);
@@ -81,30 +71,36 @@ class ManifoldDiscrimantAnalysis(ABC,Pipe):
             TTT3d = permute(reshape(TTT, [N, M, K]), [2 1 3]);
             G2 = cell2mat(arrayfun(@(x)(TTT3d(:,:,x)*U1(:,x)), 1:K, 'UniformOutput', false)).
 
-
-            """
-            
             self.G['U1'] = -G1;
             self.G['U2'] = -G2;
+        """
+        pass
     def ClassBasedDifferences(self):
-         """
-         We should implement the classbased_differences method from the MATLAB code here. 
-         """
+        """
+        We should implement the classbased_differences method from the MATLAB code here.
+        """
+        pass
     def CalculateYs(self):
         """
         Calculate Y
         """
-    def OptimizeOnManifold(self):
+        pass
+    def OptimizeOnManifold(self,options):
         if self.optmeth=='ManOpt':
-            ManifoldsOne=Stiefel(np.shape(Us[0])[0],np.shape(Us[0])[1])
-            ManifoldsTwo=Stiefel(np.shape(Us[0])[0],np.shape(Us[0])[1])
+            ManifoldOne=Stiefel(np.shape(self.Us[0])[0],np.shape(self.Us[0])[1]) #This is hardcoding it to the two-dimensional case..
+            ManifoldTwo=Stiefel(np.shape(self.Us[0])[0],np.shape(self.Us[0])[1])
             manifold=Product(ManifoldOne,ManifoldTwo)
-            problem=Problem(self.MyCost,manifold=manifold,arg=Q) #This assumes TensorFlow implementation... Else we have to implement the gradient and Hessian manually...
-            
-            solver=ConjugateGradient(problem, U, optoins)
+            Q = tf.Variable(tf.placeholder(tf.float32))
+            problem=Problem(manifold=manifold,cost=self.MyCost,arg=Q) #This assumes TensorFlow implementation... Else we have to implement the gradient and Hessian manually...
+            solver=ConjugateGradient(problem, Q, options)
+            return(solver)
     def fit(self,Xs,Ys,lowerdims=None):
         if lowerdims==None:
             self.lowerdims=np.size(Xs[0])
+        Xsample1 = Xs[0];
+        sizeX = np.shape(self.Xsample1);
+        nmodes = len(self.sizeX);
+        nsamples = np.shape(Xs);
 
     def predict(self, Xs):
         pass
@@ -114,15 +110,15 @@ class TuckerDiscriminantAnalysis(ManifoldDiscrimantAnalysis):
         if self.store[Qt]==None:
             self.store[Qt]=self.QtCalculator(Qt)
     def QtCalculator(self,Qt,N=None,K1=None,K2=None,U1=None,U2=None):
-        U1=tf.Variable(tf.Matrix)
-        U2=tf.Variable(tf.Matrix)
+        U1=tf.Variable(tf.placeholder(tf.Matrix))
+        U2=tf.Variable(tf.placeholder(tf.Matrix))
         if Qt in {'QtRw', 'QtRb'}:
             Qt_mm=tf.tensordot(tf.tensordot(self.Qt[-2:],tf.linalg.transpose(U1),axes=(1,0)),tf.linalg.transpose(U2),axes=(2,0)) #Something might be horribly wrong here...
             Qt_temp=tf.reshape(tf.roll(Qt_mm,(1,0,2))(K2*K1,N)) #Not sure if this is the correct approach. Need MATLAB license to test original behavior
         if Qt in {'QtWQ', 'QtBQ'}: #This ain't right
-            Qt_temp=tf.dot(self.store['QtRw'],self.store['QtRw']))
+            Qt_temp=tf.matmul(self.store['QtRw'],self.store['QtRw'])
         if Qt in {'QtWQinvQtBQ'}:
-            Qt_temp=tf.dot(self.store['QtWQ'],tf.linalg.inv(self.store['QtBQ'])))
+            Qt_temp=tf.matmul(self.store['QtBQ'],tf.linalg.inv(self.store['QtWQ']))
         else:
             print("Wrong Qt specified")
         self.store[Qt]=Qt_temp
