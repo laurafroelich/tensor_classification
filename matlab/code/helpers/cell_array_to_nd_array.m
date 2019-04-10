b87bee2885ad9bba3292249360a053480ad011a4
laurafroelich <laura.frolich@gmail.com>
2019-04-10 15:28:58 +0200

    Added a docstring to the function cell_array_to_nd_array.

--- matlab/code/helpers/cell_array_to_nd_array.m
+++ matlab/code/helpers/cell_array_to_nd_array.m
@@ -1,4 +1,19 @@
 function Xs_array = cell_array_to_nd_array(Xs_cell_array)
+% Xs_array = cell_array_to_nd_array(Xs_cell_array)
+%
+% Arrange n-mode arrays from cell array in (n+1)-mode array.
+%
+% Each n-mode array in the input 'Xs_cell_array' must have the same
+% dimensions. An n-mode array with observations along the (n+1)'th mode
+% is returned.
+%
+% Input:
+%
+% Xs_cell_array: Cell array containing numeric arrays of equal dimensions.
+%
+% Output:
+%
+% Xs_array: Numeric array with observations along last dimension.
     n_tensors = length(Xs_cell_array);
     obs_dimensions = size(Xs_cell_array{1});
     n_observation_modes = length(obs_dimensions);
@@ -8,7 +23,6 @@ function Xs_array = cell_array_to_nd_array(Xs_cell_array)
 
     reshaped_mat = reshape(cell_as_mat, [obs_dimensions, n_tensors]);
     
-    %permute_vector = [1:(n_observation_modes-1), n_observation_modes+1, n_observation_modes];
     permute_vector = [1:2, 4:(n_observation_modes+1), 3];
 
     permuted_mat_obs_in_last_mode = permute(reshaped_mat, permute_vector);
