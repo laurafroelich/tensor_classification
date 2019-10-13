80b9f25875e0bc3eaaa7ea2b653f0dfbd7d2eb17
laurafroelich <laura.frolich@gmail.com>
2019-10-13 08:53:08 +0200

    Add changes towards generalisation of tucker norms ratio

--- matlab/code/tensor_classification/tensorsldaobj_matrixdata_normsratio.m
+++ matlab/code/tensor_classification/tensorsldaobj_matrixdata_normsratio.m
@@ -1,6 +1,6 @@
 function [F, G, classmeandiffstensor, observationdiffstensor, store] = ...
     tensorsldaobj_matrixdata_normsratio(U,...
-    classmeandiffs, observationdiffs, nis, K1, K2,...
+    classmeandiffs, observationdiffs, nis, Ks,...
     classmeandiffstensor, observationdiffstensor, store)
 % [F, G, Ap, Bp] = tensorsldaobj_matrixdata_normsratio(U,...
 %    classmeandiffs, observationdiffs, nis, K1, K2,...
@@ -24,133 +24,65 @@ function [F, G, classmeandiffstensor, observationdiffstensor, store] = ...
 % K2:               Number of factors in mode 2.
 % classmeandiffstensor and observationdiffstensor:        Optional input. Pre-calculated matrices.
 
-if nargin < 9
-    storeexists = false;
-else
-    storeexists = true;
+[nclasses, mode_sizes, nmodes] = get_sizes(classmeandiffs, 1); % observations assumed to run along first mode
+nobs = sum(nis);
+
+Us = cell(1, nmodes);
+for imode = 1:nmodes
+    Us{imode} = U.(['U', num2str(imode)]);
 end
 
-obsexample = classmeandiffs(1,:,:);
-sizeobs = size(obsexample);
-
-I = sizeobs(2);
-J = sizeobs(3);
-U1 = U.U1;
-U2 = U.U2;
-U2t = U2';
-U1t = U1';
 if nargin <=6
-    
-    
-    permute_vector = [2:(length(sizeobs)), 1];
+    permute_vector = [2:(nmodes+1), 1];
     classmeandiffstensor = permute(classmeandiffs, permute_vector);
     observationdiffstensor = permute(observationdiffs, permute_vector);
+end
+
+
+between_classes_projected = classmeandiffstensor;
+between_obs_projected = observationdiffstensor;
+for imode = 1:nmodes
+    between_classes_projected = tmult(between_classes_projected, Us{imode}', imode);
+    between_obs_projected = tmult(between_obs_projected, Us{imode}', imode);
+end
+
+%trUtBU = sum(squeeze(sum(sum(between_classes_projected.^2, 1), 2)).*nis');
+trUtBU = sum(sum(reshape(between_classes_projected.^2, [prod(Ks), length(nis)])).*nis);
+
+% trace(U' * within_class_scatter * U)
+% trUtWU = sum(sum(sum(between_obs_projected.^2)));
+trUtWU = between_obs_projected.^2;
+trUtWU = sum(trUtWU(:));
+
+
+for imode = 1:nmodes
+    temp_projected_classes = tmult(classmeandiffstensor,Us{2}',2);
+    temp_projected_obs = tmult(observationdiffstensor,Us{2}',2);
     
-end
-
-if ~storeexists || ~isfield(store, 'Ap1')
-    Ap1 = tmult(classmeandiffstensor,U2t,2);
-    store.Ap1 = Ap1;
-else
-    Ap1 = store.Ap1;
-end
-
-if ~isfield(store, 'Bp1')
-    Bp1 = tmult(observationdiffstensor,U2t,2);
-    store.Bp1 = Bp1;
-else
-    Bp1 = store.Bp1;
-end
-
-if ~isfield(store, 'Ap2')
-    Ap2 = tmult(classmeandiffstensor,U1t,1);
-    store.Ap2 = Ap2;
-else
-    Ap2 = store.Ap2;
-end
-
-if ~isfield(store, 'Bp2')
-    Bp2 = tmult(observationdiffstensor,U1t,1);
-    store.Bp2 = Bp2;
-else
-    Bp2 = store.Bp2;
-end
-
-if ~isfield(store, 'AAp1')
-    AAp1 = tmult(Ap1,U1t,1);
-    store.AAp1 = AAp1;
-else
-    AAp1 = store.AAp1;
-end
-
-if ~isfield(store, 'BBp1')
-    BBp1 = tmult(Bp1,U1t,1);
-    store.BBp1 = BBp1;
-else
-    BBp1 = store.BBp1;
-end
-
-if ~isfield(store, 'AAp2')
-    AAp2 = tmult(Ap2,U2t,2);
-    store.AAp2 = AAp2;
-else
-    AAp2 = store.AAp2;
-end
-
-if ~isfield(store, 'BBp2')
-    BBp2 = tmult(Bp2,U2t,2);
-    store.BBp2 = BBp2;
-else
-    BBp2 = store.BBp2;
-end
-
-if ~isfield(store, 'trUtAU')
-    trUtAU = sum(squeeze(sum(sum(AAp1.^2, 1), 2)).*nis');
-    store.trUtAU = trUtAU;
-else
-    trUtAU= store.trUtAU;
-end
-if ~isfield(store, 'trUtBU')
-    trUtBU = sum(sum(sum(BBp1.^2)));
-    store.trUtBU = trUtBU;
-else
-    trUtBU = store.trUtBU;
-end
-
-if ~isfield(store, 'S1') || ~isfield(store, 'S2')
-    S1=zeros(size(U1));
-    S2=zeros(size(U2));
-    for c=1:size(Ap1,3)
-        S1=S1+Ap1(:,:,c)*AAp1(:,:,c)'*nis(c);
-        S2=S2+Ap2(:,:,c)'*AAp2(:,:,c)*nis(c);
+    Ap2 = tmult(classmeandiffstensor,Us{1}',1);
+    Bp2 = tmult(observationdiffstensor,Us{1}',1);
+    
+    S1=zeros([mode_sizes(1), Ks(1)]);
+    S2=zeros([mode_sizes(2), Ks(2)]);
+    for c=1:nclasses
+        S1=S1+temp_projected_classes(:,:,c)*between_classes_projected(:,:,c)'*nis(c);
+        S2=S2+Ap2(:,:,c)'*between_classes_projected(:,:,c)*nis(c);
     end
-    store.S1 = S1;
-    store.S2 = S2;
-else
-    S1 = store.S1;
-    S2 = store.S2;
-end
-
-if ~isfield(store, 'T1') || ~isfield(store, 'T2')
-    T1=zeros(size(U1));
-    T2=zeros(size(U2));
-    for o=1:size(Bp1,3)
-        T1=T1+Bp1(:,:,o)*BBp1(:,:,o)';
-        T2=T2+Bp2(:,:,o)'*BBp2(:,:,o);
+    
+    T1=zeros([mode_sizes(1), Ks(1)]);
+    T2=zeros([mode_sizes(2), Ks(2)]);
+    for o=1:nobs
+        T1=T1+temp_projected_obs(:,:,o)*between_obs_projected(:,:,o)';
+        T2=T2+Bp2(:,:,o)'*between_obs_projected(:,:,o);
     end
-    store.T1 = T1;
-    store.T2 = T2;
-else
-    T1 = store.T1;
-    T2 = store.T2;
+    
+    
+    G1 = -(trUtWU*2*S1-trUtBU*2*T1)/trUtWU^2;
+    G2 = -(trUtWU*2*S2-trUtBU*2*T2)/trUtWU^2;
+    G.U1 = G1;
+    G.U2 = G2;
 end
 
-
-G1 = -(trUtBU*2*S1-trUtAU*2*T1)/trUtBU^2;
-G2 = -(trUtBU*2*S2-trUtAU*2*T2)/trUtBU^2;
-G.U1 = G1;
-G.U2 = G2;
-
-F = -trUtAU/trUtBU;
+F = -trUtBU/trUtWU;
 
 end
