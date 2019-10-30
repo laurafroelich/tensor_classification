1de71a24d8a2b4d37d90b8a4249929c3f080f047
laurafroelich <laura.frolich@gmail.com>
2019-10-30 15:50:25 +0100

    Implement get_diagonal_indices using simple for loop

--- matlab/code/helpers/get_diagonal_indices.m
+++ matlab/code/helpers/get_diagonal_indices.m
@@ -6,7 +6,7 @@ function diagonal_indices = get_diagonal_indices(k, varargin)
 % Input: 
 %      k : integer, dimensionality of multidimensional cube.
 %
-% varargin{1}: number of modes on each level. Default: 2.
+% varargin{1}: number of modes in cube. Default: 2.
 %
 % Output:
 %      vector containing linear indices of diagonal
@@ -17,12 +17,36 @@ else
     n_modes = varargin{1};
 end
 
+diagonal_indices = zeros(k, 1);
+
+mat = reshape(1:(k^n_modes), repelem(k, n_modes));
+
+S.type = '()';
+    
+for ik = 1:k
+    S.subs = repmat({ik}, 1, n_modes);
+    diagonal_indices(ik, 1) = subsref(mat, S);
+end
+
+% attempts at vectorising function to improve speed
+if false
 if n_modes == k
     diagonal_indices = diag(cumsum([1:(k+1):k^2; k^2.*ones(k-1,k)]));
+else
+    if n_modes == 2
+        diagonal_indices = (1:(k+1):k^2)';
+    else
+        diagonal_indices = diag(cumsum([1:(k^(n_modes-1)+1):k^n_modes; k*(k+1).*ones(k-1,k)]));
+        
+        % k: 2, n_modes: 4                                             3*2
+        %diagonal_indices = diag(cumsum([1:(k^(n_modes-1)+1):k^n_modes; factorial(n_modes-(n_modes-k)+1).*ones(k-1,k)]));
+        
+        % k: 3, n_modes:4                                             3*2*2
+        %diagonal_indices = diag(cumsum([1:(k^(n_modes-1)+1):k^n_modes; ((k*n_modes)).*ones(k-1,k)]));
+        
+
+    end
 end
 
-if n_modes == 2
-    diagonal_indices = (1:(k+1):k^2)';
 end
-
 end
--- matlab/code/tests/test_get_diagonal_indices.m
+++ matlab/code/tests/test_get_diagonal_indices.m
@@ -25,3 +25,74 @@ function test_diagonal_indices_2d_dim5(testCase)
     
     assert(isequal(diag_elems, [17, 5, 13, 21, 9]'))
 end
+
+function test_diagonal_indices_3d_dim3(testCase)
+    import matlab.unittest.fixtures.PathFixture
+    testCase.applyFixture(PathFixture('../', 'IncludeSubfolders', true));
+
+    mat = reshape(1:27, [3, 3, 3]);
+    diag_inds = get_diagonal_indices(3, 3);
+    diag_elems = mat(diag_inds);
+    
+    assert(isequal(diag_elems, [1, 14, 27]'))
+end
+
+
+function test_diagonal_indices_4d_dim3(testCase)
+    import matlab.unittest.fixtures.PathFixture
+    testCase.applyFixture(PathFixture('../', 'IncludeSubfolders', true));
+
+    mat = reshape(1:81, [3, 3, 3, 3]);
+    diag_inds = get_diagonal_indices(3, 4);
+    diag_elems = mat(diag_inds);
+    
+    assert(isequal(diag_elems, [1, 41, 81]'))
+end
+
+function test_diagonal_indices_4d_dim2(testCase)
+    import matlab.unittest.fixtures.PathFixture
+    testCase.applyFixture(PathFixture('../', 'IncludeSubfolders', true));
+
+    mat = reshape(1:(2^4), [2, 2, 2, 2]);
+    diag_inds = get_diagonal_indices(2, 4);
+    diag_elems = mat(diag_inds);
+    
+    assert(isequal(diag_elems, [1, 16]'))
+end
+
+
+
+function test_diagonal_indices_6d_dim3(testCase)
+    import matlab.unittest.fixtures.PathFixture
+    testCase.applyFixture(PathFixture('../', 'IncludeSubfolders', true));
+
+    mat = reshape(1:(3^6), [3, 3, 3, 3, 3, 3]);
+    diag_inds = get_diagonal_indices(3, 6);
+    diag_elems = mat(diag_inds);
+    
+    assert(isequal(diag_elems, [1, 365, 729]'))
+end
+
+
+function test_diagonal_indices_3d_dim5(testCase)
+    import matlab.unittest.fixtures.PathFixture
+    testCase.applyFixture(PathFixture('../', 'IncludeSubfolders', true));
+
+    mat = reshape(1:(5^3), [5, 5, 5]);
+    diag_inds = get_diagonal_indices(5, 3);
+    diag_elems = mat(diag_inds);
+    
+    assert(isequal(diag_elems, [1, 32, 63, 94, 125]'))
+end
+
+
+function test_diagonal_indices_4d_dim7(testCase)
+    import matlab.unittest.fixtures.PathFixture
+    testCase.applyFixture(PathFixture('../', 'IncludeSubfolders', true));
+
+    mat = reshape(1:(7^4), [7, 7, 7, 7]);
+    diag_inds = get_diagonal_indices(7, 4);
+    diag_elems = mat(diag_inds);
+    
+    assert(isequal(diag_elems, [1, 401, 801, 1201, 1601, 2001, 2401]'))
+end
